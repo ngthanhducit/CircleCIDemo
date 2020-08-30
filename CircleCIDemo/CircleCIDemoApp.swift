60dbdb86621ea2a3814ada69d4f9aed53fd5bc92
ngthanhducit <thanhducniit@yahoo.com>
2020-08-30 19:11:03 +0700

    update

---
+++ CircleCIDemo/CircleCIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleCIDemoApp.swift
+//  CircleCIDemo
+//
+//  Created by Nguyen Thanh Duc on 8/30/20.
+//
+
+import SwiftUI
+
+@main
+struct CircleCIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-CircleCIDemo-CircleCIDemoUITests/Pods-CircleCIDemo-CircleCIDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CircleCIDemo_CircleCIDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CircleCIDemo_CircleCIDemoUITests
+@end
