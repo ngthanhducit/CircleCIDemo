a00bf695d807592ffd8bf624c9c33513e0e18991
ngthanhducit <thanhducniit@yahoo.com>
2020-09-25 23:04:14 +0700

    update

--- CircleCIDemo/CircleCIDemoUITests/CircleCIDemoUITests.swift
+++ CircleCIDemo/CircleCIDemoUITests/CircleCIDemoUITests.swift
@@ -26,6 +26,10 @@ class CircleCIDemoUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        let i = 0
+        
+        XCTAssert(i == 0, "I Support to be 0")
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
